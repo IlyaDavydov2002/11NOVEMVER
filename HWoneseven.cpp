89f171892290e0929caa40ef415a73c2547721b5
IlyaDavydov2002 <44145869+IlyaDavydov2002@users.noreply.github.com>
2018-11-12 00:15:44 +0300

    Add files via upload

---
+++ HWoneseven.cpp
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int const n = 11;
+	int A[n][n];
+	int i = 1, j, k, p = n / 2;
+	for (k = 1; k <= p; k++)
+	{
+		for (j = k - 1; j < n - k + 1; j++) A[k - 1][j] = i++;
+		for (j = k; j < n - k + 1; j++) A[j][n - k] = i++;
+		for (j = n - k - 1; j >= k - 1; --j) A[n - k][j] = i++;
+		for (j = n - k - 1; j >= k; j--) A[j][k - 1] = i++;
+	}
+	if (n % 2 == 1) A[p][p] = n * n;
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
+		{
+			printf(" %4d ", A[i][j]);
+			if (j == n - 1) printf("\n");
+		}
+}
